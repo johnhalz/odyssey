d2ddda78ed51683a3364ec695b4e6017d9603711
John Halazonetis <john.halazonetis@icloud.com>
2025-04-23 19:00:45 +0200

    Added User model

---
+++ Sources/odyssey/DTOs/UserDTO.swift
@@ -0,0 +1,42 @@
+//
+//  UserDTO.swift
+//  odyssey
+//
+//  Created by John Halazonetis on 21.04.2025.
+//
+
+import Fluent
+import Vapor
+
+extension User {
+    struct Create: Content {
+        var firstName: String
+        var lastName: String
+        var email: String
+        var password: String
+        var confirmPassword: String
+    }
+}
+
+extension User.Create: Validatable {
+    static func validations(_ validations: inout Validations) {
+        validations.add("firstName", as: String.self, is: !.empty)
+        validations.add("lastName", as: String.self, is: !.empty)
+        validations.add("email", as: String.self, is: .email)
+        validations.add("password", as: String.self, is: .count(8...))
+    }
+}
+
+extension User: ModelAuthenticatable {
+    static var usernameKey: KeyPath<User, Field<String>> {
+        \User.$email
+    }
+    
+    static var passwordHashKey: KeyPath<User, Field<String>> {
+        \User.$passwordHash
+    }
+    
+    func verify(password: String) throws -> Bool {
+        try Bcrypt.verify(password, created: self.passwordHash)
+    }
+}
--- Sources/odyssey/routes.swift
+++ Sources/odyssey/routes.swift
@@ -11,4 +11,30 @@ func routes(_ app: Application) throws {
     }
 
     try app.register(collection: TodoController())
+    
+    // User
+    app.post("users") { req async throws -> User in
+        try User.Create.validate(content: req)
+        let create = try req.content.decode(User.Create.self)
+        guard create.password == create.confirmPassword else {
+            throw Abort(.badRequest, reason: "Passwords did not match")
+        }
+        
+        let user = try User(
+            firstName: create.firstName,
+            lastName: create.lastName,
+            email: create.email,
+            passwordHash: Bcrypt.hash(create.password)
+        )
+        
+        try await user.save(on: req.db)
+        return user
+    }
+    
+    let passwordProtected = app.grouped(User.authenticator())
+    passwordProtected.post("login") { req async throws -> User in
+        try req.auth.require(User.self)
+    }
 }
+
+
