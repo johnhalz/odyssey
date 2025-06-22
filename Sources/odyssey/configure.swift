3ac33c3084e7324678709662325f314de054ed98
John Halazonetis <john.halazonetis@icloud.com>
2025-06-22 19:07:35 +0200

    Added encoding of json data to be in snake_case

--- Sources/odyssey/configure.swift
+++ Sources/odyssey/configure.swift
@@ -1,6 +1,6 @@
-import NIOSSL
 import Fluent
 import FluentPostgresDriver
+import NIOSSL
 import Vapor
 
 // Configuration
@@ -8,14 +8,30 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
-        password: Environment.get("DATABASE_PASSWORD") ?? "odysseypassword",
-        database: Environment.get("DATABASE_NAME") ?? "postgres",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
+    // Configure JSON encoder to use snake_case
+    let encoder = JSONEncoder()
+    encoder.keyEncodingStrategy = .convertToSnakeCase
+    encoder.dateEncodingStrategy = .iso8601
+
+    // Configure JSON decoder to use snake_case
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    decoder.dateDecodingStrategy = .iso8601
+
+    ContentConfiguration.global.use(encoder: encoder, for: .json)
+    ContentConfiguration.global.use(decoder: decoder, for: .json)
+
+    app.databases.use(
+        DatabaseConfigurationFactory.postgres(
+            configuration: .init(
+                hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+                port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:))
+                    ?? SQLPostgresConfiguration.ianaPortNumber,
+                username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+                password: Environment.get("DATABASE_PASSWORD") ?? "odysseypassword",
+                database: Environment.get("DATABASE_NAME") ?? "postgres",
+                tls: .prefer(try .init(configuration: .clientDefault)))
+        ), as: .psql)
 
     app.migrations.add(User.Migration())
     app.migrations.add(UserToken.Migration())
